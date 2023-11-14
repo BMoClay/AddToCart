9855700bd46a75d5d1d8f1c202e7d43f9c85f0ca
BMoClay <benmorgancleveland@gmail.com>
2023-11-13 20:26:46 -0500

    brown version change moviesInDb to things

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-776fe-default-rtdb.firebaseio.com/"
+}
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const moviesInDB = ref(database, "things")
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
-    console.log(inputValue)
+    push(moviesInDB, inputValue)
+    
+    console.log(`${inputValue} added to database`)
 })
