d0a62bc508ae3e74f6879ce26657fbea23f838e2
BMoClay <benmorgancleveland@gmail.com>
2023-11-15 17:59:34 -0500

    finish playing around

--- index.js
+++ index.js
@@ -1,23 +1,142 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js"
-import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js"
 const appSettings = {
-    databaseURL:"https://playground-776fe-default-rtdb.firebaseio.com/"
+    databaseURL:"https://books-10a3a-default-rtdb.firebaseio.com/"
 }
-
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const booksInDB = ref(database, "books")
+const shoppingListInDB = ref(database, "shoppingList")
 
-const booksEl = document.getElementById("books")
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
-function clearBooksListEl() {
-    booksEl.innerHTML = ""
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    if (inputValue != "") {
+        push(shoppingListInDB, inputValue);
+    }
+    
+    clearInputFieldEl()
+})
+
+onValue( shoppingListInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        let itemsArray = Object.entries(snapshot.val())
+    
+        clearShoppingListEl() 
+    
+        for ( let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+    
+            appendItemToShoppingListEl(currentItem)
+        }
+    } else {
+        shoppingListEl.innerHTML = "nothing here yet"
+    }
+    
+})
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
 }
 
-function appendBookToBooksListEl(bookValue) {
-    booksEl.innerHTML += `<li>${bookValue}</li>`
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
 }
+
+function appendItemToShoppingListEl(item) {
+    // shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+    let itemID = item[0]
+    let itemValue = item[1]
+
+    let newEl = document.createElement("li")
+
+    newEl.textContent = itemValue
+
+    newEl.addEventListener("dblclick", function() {
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+       
+        remove(exactLocationOfItemInDB)
+    })
+
+    shoppingListEl.append(newEl)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js"
+// import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js"
+// const appSettings = {
+//     databaseURL:"https://books-10a3a-default-rtdb.firebaseio.com/"
+// }
+// const app = initializeApp(appSettings)
+// const database = getDatabase(app)
+// const newsStoriesInDB = ref(database, "shoppingList")
+
+// const storiesEl = document.getElementById("stories")
+
+// onValue(newsStoriesInDB, function(snapshot) {
+//     let newsStoriesArray = Object.entries(snapshot.val())
+    
+//     storiesEl.innerHTML = ""
+    
+//     for (let i = 0; i < newsStoriesArray.length; i++) {
+//         let currentStory = newsStoriesArray[i]
+        
+//         appendStoryToStoriesEl(currentStory)
+//     }
+// })
+
+// function appendStoryToStoriesEl(story) {
+//     let storyID = story[0]
+//     let storyTitle = story[1]
+    
+//     let newEl = document.createElement("div")
+    
+//     newEl.classList.add("story")
+    
+//     newEl.textContent = storyTitle
+
+//     newEl.addEventListener("dblclick", function() {
+//         let exactLocationOfStoryInDB = ref(database, `shoppingList/${storyID}`)
+        
+//         remove(exactLocationOfStoryInDB)
+//     })
+    
+//     storiesEl.append(newEl)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // const inputFieldEl = document.getElementById("input-field")
 // const addButtonEl = document.getElementById("add-button")
